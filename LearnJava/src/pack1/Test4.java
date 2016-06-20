67747676cd7c8fc148e0eb9d2430f71591af957b
mengluo <571059757@qq.com>
2016-06-20 13:29:16 +0800

    基本数据类型

---
+++ LearnJava/src/pack1/Test4.java
@@ -0,0 +1,20 @@
+package pack1;
+/**
+ * 
+ * 一.Java语言基础  ---> Java基础语法 ---> 基本数据类型
+ * 
+ * 	重点
+ * 		一.八大基本数据类型
+ * 			
+ * 		1.byte	2.short	3.int 4.long 5.float 6.double 7.char 8.boolean
+ * 
+ * 		二.类型转换
+ * 
+ * 		byte ---> short --->int ---> long ---> float ---> double
+ * 
+ * 		原理：小的类型都包含，大的肯定包含，可以自动类型转换(从左到右)
+ * 			 大的类型包含，放到小的类型里面不一定包含，需要强制类型转换(从右到左)
+ */
+public class Test4 {
+	
+}
