1eda41fefacd3ecec70c0d9d3d99077803b4410a
mengluo <571059757@qq.com>
2016-06-21 11:19:00 +0800

    多维数组

---
+++ LearnJava/src/pack2/Test6.java
@@ -0,0 +1,26 @@
+package pack2;
+/**
+  	一.Java语言基础  ---> 数组 ---> 多维数组
+ 
+ 		重点
+ 
+ 		1.二维数组
+ 
+  			A.基本概念
+  
+	  			a.二维数组是由多个一维数组组成
+				b.每个一维数组都是二维数组的一个元素
+				c.二维数组的长度就是其拥有一维数组的个数
+		
+		2.三维数组
+
+ */
+public class Test6 {
+	
+	public static void main(String[] args) {
+		
+		
+		
+	}
+	
+}
